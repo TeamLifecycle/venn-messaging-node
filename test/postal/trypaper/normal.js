2d28ca9db466af83c25e830c68f5a0c1cc193391
Nathan Ashcraft <ashcrang@mail.uc.edu>
2015-06-17 11:29:45 -0400

    TryPaper functionality with tests. for #51

--- test/postal/trypaper/normal.js
+++ test/postal/trypaper/normal.js
@@ -29,18 +29,33 @@ describe('TryPaper success', function () {
 	it('should send successfully with proper data', function (done) {
 
 		var letter = {
-
+			to: {
+				"Name": "John Smith",
+			    "Organization": "ACME, Inc.",
+			    "AddressLineOne": "123 Any Street",
+			    "AddressLineTwo": "Suite 789",
+			    "City": "Anytown",
+			    "Province": "WA",
+			    "PostalCode": "12345",
+			    "Country": "US"
+			},
+			from: 'Return_Address_Id_String',
+			file: "http://i.trypaper.com/pub/MacroniAndCheeseReference.pdf",
+			tags: ["duplex", "force_bw"],
+			HIPAASensitive: false,
+			batchId: "Feb 2013 Invoices",
+			id: "accountId"
 		};
 
 		nock('https://api.trypaper.com')
 			.post('/Mailing')
-			.reply(201, {'message': 'TODO: Find out what should go here!'});
+			.reply(201, {'message': 'Success!'});
 
 		client.initialize()
 		client.send(letter, function(err, result) {
-		
-			// TODO: assertions here
-
+			should.not.exist(err);
+			should.exist(result);
+			result.service.should.equal('trypaper');
 			done()
 		})
 	})
